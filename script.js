e08af92261b79b7eef35de67226bbb8e4f8a4905
Tushar Sharma <ts6261423@gmail.com>
2022-05-01 16:56:52 +0530

    updated

---
+++ script.js
@@ -0,0 +1,29 @@
+// the villa button section
+
+function btn() {
+    let image = document.getElementById('image');
+    if(image.src="images/pic13.jpg"){
+        image.src="images/pic4.jpg"
+    }
+    
+}
+function btn4() {
+    let image2 = document.getElementById('image2');
+    if(image2.src="images/pic11.jpg"){
+        image2.src="images/pic1.jpg"
+    }
+    
+}
+function btn5() {
+    let image3 = document.getElementById('image3');
+    if(image3.src="images/pic14.jpg"){
+        image3.src="images/pic12.jpg"
+    }
+    
+}
+
+
+// active classes
+
+
+
